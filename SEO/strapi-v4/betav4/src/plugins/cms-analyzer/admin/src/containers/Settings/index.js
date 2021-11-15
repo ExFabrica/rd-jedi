c3fd55e95758896293e3c49740a0e0d5965e2d00
GTA <gregory.taudin@exfrabrica.io>
2021-11-15 11:09:39 +0100

    - Add & revamp settings:  -> use new UI controls and layout components from new V4 Ui library

--- SEO/strapi-v4/betav4/src/plugins/cms-analyzer/admin/src/containers/Settings/index.js
+++ SEO/strapi-v4/betav4/src/plugins/cms-analyzer/admin/src/containers/Settings/index.js
@@ -1,23 +1,37 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { useIntl } from 'react-intl';
-import { Header } from '@buffetjs/custom';
-import { Button, Toggle, InputText, Label } from '@buffetjs/core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import getTrad from '../../utils/getTrad';
 import SettingsMiddleware from '../../middlewares/settings/ui-settings';
+import { Stack } from '@strapi/design-system/Stack';
+import { Main } from '@strapi/design-system/Main';
+import { ContentLayout } from '@strapi/design-system/Layout';
+import { Box } from '@strapi/design-system/Box';
+import { H2 } from '@strapi/design-system/Text';
+import { TextInput } from '@strapi/design-system/TextInput';
+import { Button } from '@strapi/design-system/Button';
+import Plus from '@strapi/icons/Plus';
+import { HeaderLayout } from '@strapi/design-system/Layout';
+import { Grid, GridItem } from '@strapi/design-system/Grid';
+import {
+  CheckPagePermissions,
+  LoadingIndicatorPage,
+  useNotification,
+} from '@strapi/helper-plugin';
+import { ToggleInput } from '@strapi/design-system/ToggleInput';
 
 const SettingsPage = () => {
   const { formatMessage } = useIntl();
   const isMounted = useRef(true);
-  const [showLoader, setShowLoader] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [settings, setSettings] = useState();
+  const toggleNotification = useNotification();
+
   //mount
   useEffect(() => {
     SettingsMiddleware.get().then((data) => {
       setSettings(data);
       console.log('data', data);
-      setShowLoader(false);
+      setIsLoading(false);
     });
     // unmount
     return () => {
@@ -26,81 +40,95 @@ const SettingsPage = () => {
   }, []);
 
   const handleSubmit = async () => {
-    setShowLoader(true);
+    setIsLoading(true);
     const data = await SettingsMiddleware.set(settings);
     setSettings(data);
-    setShowLoader(false);
-    strapi.notification.success(formatMessage({ id: getTrad("plugin.settings.button.save.message") }));
+    setIsLoading(false);
+    toggleNotification({
+      type: 'success',
+      message: { id: getTrad("plugin.settings.button.save.message") },
+    });
   }
+
+  //TODO ADD the RBAC controls
+  //<CheckPermissions permissions={permissions.createRole}>
+  //</CheckPermissions>
 
   return (
     <>
-      <div className="row">
-        <div className="col-6">
-          <Header
-            title={{ label: formatMessage({ id: getTrad("plugin.settings.title") }) }}
-            content={formatMessage({ id: getTrad("plugin.settings.subtitle") })}
-            isLoading={showLoader}
-          />
-        </div>
-        <div className="col-6" style={{ textAlign: 'right' }}>
-          <Button color="primary" onClick={handleSubmit} icon={<FontAwesomeIcon icon={faPlus} />} label={formatMessage({ id: getTrad("plugin.settings.button.save.label") })} />
-        </div>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <div
-          title={formatMessage({ id: getTrad("plugin.settings.panel.title") })}
-          isLoading={showLoader}
+      <Main labelledBy="title" aria-busy={isLoading}>
+        <HeaderLayout
+          id="title"
+          title={formatMessage({ id: getTrad("plugin.settings.title") })}
+          subtitle={formatMessage({ id: getTrad("plugin.settings.subtitle") })}
+          primaryAction={
+            <Button onClick={handleSubmit} startIcon={<Plus />} size="L" >
+              {formatMessage({ id: getTrad("plugin.settings.button.save.label") })}
+            </Button>
+          }
         >
-          <div className="col-12 row">
-            <div className="col-12">
-              <Label htmlFor="default-string" message={formatMessage({ id: getTrad("plugin.settings.panel.setting1.label") })} />
-              <InputText
-                label="Label"
-                name="default-string"
-                placeholder={formatMessage({ id: getTrad("plugin.settings.panel.setting1.placeholder") })}
-                onChange={({ target: { value } }) => {
-                  setSettings((prevState) => {
-                    return { ...prevState, frontUrl: value };
-                  });
-                }}
-                value={settings && settings.frontUrl ? settings.frontUrl : ""}
-              />
-            </div>
-            {/*<div className="col-6">
-              <Label htmlFor="default-number" message={formatMessage({ id: getTrad("plugin.settings.panel.setting3.label") })} />
-              <InputNumber
-                label="Label"
-                name="default-number"
-                onChange={({ target: { value } }) => {
-                  setSettings((prevState) => {
-                    return { ...prevState, setting3: value };
-                  });
-                }}
-                value={settings && settings.setting3 ? settings.setting3 : 0}
-              />
-              </div>*/}
-          </div>
-          <div className="col-12 row" style={{ marginTop: "20px" }}>
-            <div className="col-6">
-              <Label htmlFor="default-toggle" message={formatMessage({ id: getTrad("plugin.settings.panel.setting2.label") })} />
-              <Toggle
-                name="default-toggle"
-                onChange={({ target: { value } }) => {
-                  setSettings((prevState) => {
-                    return { ...prevState, enabled: value };
-                  });
-                }}
-                value={settings && settings.enabled ? settings.enabled : false}
-              />
-            </div>
-            <div className="col-6">
-            </div>
-          </div>
-          <div className="col-12 row" style={{ marginTop: "20px" }}>
-          </div>
-        </div>
-      </form>
+        </HeaderLayout>
+        {isLoading ? (<LoadingIndicatorPage />)
+          : <ContentLayout>
+            <form onSubmit={handleSubmit}>
+              <Box
+                background="neutral0"
+                hasRadius
+                shadow="filterShadow"
+                paddingTop={6}
+                paddingBottom={6}
+                paddingLeft={7}
+                paddingRight={7}
+              >
+                <Stack size={3}>
+                  <H2>
+                    {formatMessage({
+                      id: getTrad("plugin.settings.panel.title")
+                    })}
+                  </H2>
+                  <Grid gap={6}>
+                    <GridItem col={6} s={12}>
+                      <TextInput
+                        label="Front-end URL to analyze"
+                        name="siteURI"
+                        placeholder={formatMessage({ id: getTrad("plugin.settings.panel.setting1.placeholder") })}
+                        onChange={({ target: { value } }) => {
+                          setSettings((prevState) => {
+                            return { ...prevState, frontUrl: value };
+                          });
+                        }}
+                        value={settings && settings.frontUrl ? settings.frontUrl : ""}
+                        hint={'The URL of the frontend you want to analyze'}
+                      />
+                    </GridItem>
+                    <GridItem col={6} s={12}>
+                      <ToggleInput
+                        aria-label="crawling"
+                        data-testid="crawling"
+                        checked={settings && settings.hasOwnProperty("enabled") ? settings.enabled : false}
+                        hint={'Enable or disable the analyze of this frontend'}
+                        label={'Enabled?'}
+                        name="siteEnabled"
+                        offLabel={formatMessage({
+                          id: 'app.components.ToggleCheckbox.off-label',
+                          defaultMessage: 'Off',
+                        })}
+                        onLabel={formatMessage({
+                          id: 'app.components.ToggleCheckbox.on-label',
+                          defaultMessage: 'On',
+                        })}
+                        onChange={e => {
+                          setSettings({ ...settings, enabled: e.target.checked });
+                        }}
+                      />
+                    </GridItem>
+                  </Grid>
+                </Stack>
+              </Box>
+            </form>
+          </ContentLayout>
+        }
+      </Main>
     </>
   );
 };
