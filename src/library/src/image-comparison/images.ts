cb98a2173cf836ea90ed4fda5c180fb371c4b80e
Nicolas Malburet <nicolas.malburet@gmail.com>
2021-12-23 17:11:23 +0100

    analyze images from name

--- src/library/src/image-comparison/images.ts
+++ src/library/src/image-comparison/images.ts
@@ -6,41 +6,62 @@ import fs from "mz/fs";
 export default async function startImageAnalysis(): Promise<any> {
     const frontPath = "./src/image-comparison/front";
     const cmsPath = "./src/image-comparison/cms";
-    fs.readdir(frontPath, function (frontErr, frontFiles) {
-        if (frontErr) return console.log('Unable to scan directory: ' + frontErr);
-        
-        frontFiles.forEach(function (frontFile) {
-            fs.readdir(cmsPath, async function (cmsErr, cmsFiles) {
-            if (cmsErr) return console.log('Unable to scan directory: ' + cmsErr);
-            
-            const file1 = await fs.readFile(frontPath + "/" + frontFile);
-            cmsFiles.forEach(async function (cmsFile) {
-                const file2 = await fs.readFile(cmsPath + "/" + cmsFile);
+    //get cms images
+    fs.readdir(cmsPath, function (cmsErr, cmsFiles) {
+        if (cmsErr) return console.log('Unable to scan directory: ' + cmsErr);
+        //get front images
+        fs.readdir(frontPath, async function (frontErr, frontFiles) {
+            if (frontErr) return console.log('Unable to scan directory: ' + frontErr);
+            //Analyzing cms images
+            cmsFiles.forEach(async function (cmsFilePath) {
+                const cmsFile = await fs.readFile(cmsPath + "/" + cmsFilePath);
+                const cmsFileName = getFileName(cmsFilePath);
+                let imageFound = false;
 
-                const options = {
-                    returnEarlyThreshold: 5,
-                    scaleToSameSize: true,
-                    ignoreAntialiasing: true,
-                    ignoreColors: true
-                };
-
-                //1. resemblejs
-                compare(file1, file2, options, function (err, data) {
-                    if (err) {
-                        console.log(err);
-                        //const file1_redim = sharp(file1).resize({width: 300, height: 200}).toBuffer()
-                        //const file2_redim = sharp(file2).resize({width: 300, height: 200}).toBuffer()
-
-                        //2. Node-resemble
-                        // const diff = resemble(file1).compareTo(file2).ignoreColors().onComplete(function(data){
-                        //     console.log("Front : " + frontFile + " Cms: " + cmsFile  + " match: " + data.misMatchPercentage);
-                        // });
-                    } else {
-                        console.log("Front : " + frontFile + " Cms: " + cmsFile  + " match: " + data.misMatchPercentage);
+                frontFiles.forEach(async function (frontFilePath) {
+                    if(!imageFound){
+                        //1. Compare file names
+                        const frontFileName = getFileName(frontFilePath);
+                        if(frontFileName.includes(cmsFileName)){
+                            console.log("Found same file: " + cmsFileName)
+                            imageFound = true;
+                        }
                     }
                 });
+
+                if(!imageFound){
+                    frontFiles.forEach(async function (frontFilePath) {
+                        if(!imageFound){
+                        //2. If not found, compare 
+                            const frontFile = await fs.readFile(frontPath + "/" + frontFilePath);
+                            compare(cmsFile, frontFile, options, function (err, data) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    console.log("Front : " + frontFilePath + " Cms: " + cmsFilePath  + " match: " + data.misMatchPercentage);
+                                    if(data.misMatchPercentage < 4){
+                                        console.log("Found same file: " + cmsFileName)
+                                        imageFound = true;
+                                    }
+                                }
+                            });
+                        }
+                    });
+                }
             });
-        });
         });
     });
 }
+
+const getFileName = (file: string): string => {
+    const parts = file.split('.');
+    const ext = parts[parts.length-1]; 
+    return parts.splice(0,parts.length-1).join(".");
+}
+
+const options = {
+    returnEarlyThreshold: 5,
+    scaleToSameSize: true,
+    ignoreAntialiasing: true,
+    ignoreColors: true
+};
